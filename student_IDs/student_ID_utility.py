befd4bcaa04e536eb700a802bbbb0895f58696d9
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2020-11-19 20:18:17 -0700

    initial commit

--- student_IDs/student_ID_utility.py
+++ student_IDs/student_ID_utility.py
@@ -1,9 +1,14 @@
 # STANDARD LIBRARY IMPORTS
 try:
     import tkinter as tk
+    import tkinter.font as tkFont
+    import tkinter.ttk as ttk
 except ImportError:
     import Tkinter as tk
+    import tkFont
+    import ttk
 from tkinter import messagebox
+
 
 # CUSTOM LIBRARY IMPORTS
 
@@ -21,11 +26,31 @@ class GUI(tk.Frame):
         super().__init__(master)
         self.master = master
         master.title("Student ID Utility")
-        master.geometry("700x425")
+        # master.geometry("700x450")
         master['bg'] = COLORS['antique_white']['py_name']
+
+        self.frame_leftTop = tk.Frame(master, bg="red", height=225, width=150)
+        self.frame_leftBottom = tk.Frame(
+            master, bg="green", height=225, width=150)
+        self.frame_rightFull = tk.Frame(
+            master, bg="blue", height=150, width=350)
+
+        # self.frame_leftTop.grid(row=5, column=5, rowspan=15, columnspan=10, sticky=tk.N+tk.W)
+        # self.frame_leftBottom.grid(row=20, column=5, rowspan=15, columnspan=10, sticky=tk.S+tk.W)
+        # self.frame_rightFull.grid(row=5, column=20, rowspan=40, columnspan=40, sticky=tk.E)
+
+        self.frame_rightFull.pack(side="right", fill="both")
+        self.frame_leftTop.pack(side="top", fill="both")
+        self.frame_leftBottom.pack(
+            side="bottom", fill="both", after=self.frame_leftTop)
+        
+        self.tree = None
 
 
 if __name__ == '__main__':
+    size = { "width": 700, "height": 450}
     root = tk.Tk()
+    root.minsize(size["width"], size["height"])
+    root.maxsize(size["width"], size["height"])
     gui = GUI(master=root)
     gui.mainloop()
