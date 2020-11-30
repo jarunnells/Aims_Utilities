264a895e1ae5827abcbb40526c30bce2523e11f0
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2020-11-30 05:54:36 -0700

    testing new temp dir naming convention

--- student_IDs/student_ID_utility_GUI_TEST.py
+++ student_IDs/student_ID_utility_GUI_TEST.py
@@ -1,32 +1,42 @@
 # STANDARD LIBRARY IMPORTS
-try:
-    import tkinter as tk
-    import tkinter.font as tkFont
-    import tkinter.ttk as ttk
-except ImportError:
-    import Tkinter as tk
-    import tkFont
-    import ttk
-from tkinter import messagebox
+import tkinter as tk
+import tkinter.font as tk_font  # tkFont
+from tkinter import (ttk, messagebox)
 
+# THIRD PARTY IMPORTS
 
-# CUSTOM LIBRARY IMPORTS
+# LOCAL IMPORTS
 
 COLORS = {
-    "light_steel_blue": {"py_name": "light steel blue", "hex": "#B0C4DE", "rgb": (176, 196, 222)},
-    "light_coral": {"py_name": "light coral", "hex": "#F08080", "rgb": (240, 128, 128)},
-    "lavender": {"py_name": "lavender", "hex": "#E6E6FA", "rgb": (230, 230, 250)},
-    "antique_white": {"py_name": "antique white", "hex": "#FAEBD7", "rgb": (250, 235, 215)},
+    "light_steel_blue": {
+        "py_name": "light steel blue",
+        "hex": "#B0C4DE",
+        },
+    "light_coral": {
+        "py_name": "light coral",
+        "hex": "#F08080",
+        },
+    "lavender": {
+        "py_name": "lavender",
+        "hex": "#E6E6FA",
+        },
+    "antique_white": {
+        "py_name": "antique white",
+        "hex": "#FAEBD7",
+        },
 }
+
+GUI_DIM = {"width": 700, "height": 450}
 
 
 class GUI(tk.Frame):
-
+    global GUI_DIM
     def __init__(self, master):
         super().__init__(master)
         self.master = master
         master.title("Student ID Utility")
-        # master.geometry("700x450")
+        master.resizable(width=False, height=False)
+        master.geometry(width=GUI_DIM['width'], height=GUI_DIM['height'])
         master['bg'] = COLORS['antique_white']['py_name']
 
         self.frame_leftTop = tk.Frame(master, bg="red", height=225, width=150)
@@ -43,14 +53,12 @@ class GUI(tk.Frame):
         self.frame_leftTop.pack(side="top", fill="both")
         self.frame_leftBottom.pack(
             side="bottom", fill="both", after=self.frame_leftTop)
-        
+
         self.tree = None
 
 
 if __name__ == '__main__':
-    size = { "width": 700, "height": 450}
     root = tk.Tk()
-    root.minsize(size["width"], size["height"])
-    root.maxsize(size["width"], size["height"])
+    root.option_add('*tearOff', False)
     gui = GUI(master=root)
     gui.mainloop()
