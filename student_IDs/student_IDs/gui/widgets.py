caf2ec89736fd56ecfa386271246042ee00d6aee
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2020-12-04 16:42:02 -0700

    cont'd dev

--- student_IDs/student_IDs/gui/widgets.py
+++ student_IDs/student_IDs/gui/widgets.py
@@ -13,23 +13,53 @@ from tkinter import (ttk, messagebox)
 
 class GUI_Window:
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, parent, title, geometry, resizable, background,
+                 *args, **kwargs):
+        self.parent = parent
+        self.parent.title(title)
+        self.parent.geometry(geometry)  # 'WIDTHxHEIGHT+X+Y'
+        self.parent.resizable(resizable)
+        self.parent.mainloop()
 
 
 class GUI_Button:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, parent, text, width, justify, state, underline,
+                 textVariable, style, command, *args, **kwargs):
+        self.button = ttk.Button(self, parent)
         pass
 
 
 class GUI_Label:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, parent, *args, **kwargs):
+        self.label = ttk.Label(self, parent)
         pass
 
 
 class GUI_Entry:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, parent, *args, **kwargs):
+        self.entry = ttk.Entry(self, parent)
+        pass
+
+
+class GUI_Combobox:
+
+    def __init__(self, parent, *args, **kwargs):
+        self.entry = ttk.Combobox(self, parent)
+        pass
+
+
+class GUI_Scrollbar:
+
+    def __init__(self, parent, *args, **kwargs):
+        self.scrollbar = ttk.Scrollbar(self, parent)
+        pass
+
+
+class GUI_Progressbar:
+
+    def __init__(self, parent, *args, **kwargs):
+        self.scrollbar = ttk.Progressbar(self, parent)
         pass
